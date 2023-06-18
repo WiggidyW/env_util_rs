36484dc99505be4565312aea8f50fbf5da684773
Wiggidy <rlr0017@auburn.edu>
2023-06-18 18:57:23 -0500

    added stderror placeholders

--- src/error.rs
+++ src/error.rs
@@ -1,23 +1,81 @@
 use std::{
     error::Error as StdError,
-    ffi::OsString,
+    fmt::Display,
 };
 
+#[derive(Debug)]
 pub enum Error {
-    Missing(MissingError),
     InvalidUnicode(InvalidUnicodeError),
+    Missing(MissingError),
     Parse(ParseError),
 }
 
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidUnicode(e) => e.fmt(f),
+            Self::Missing(e) => e.fmt(f),
+            Self::Parse(e) => e.fmt(f),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            Self::InvalidUnicode(e) => Some(e),
+            Self::Missing(e) => Some(e),
+            Self::Parse(e) => Some(e),
+        }
+    }
+}
+
+impl From<MissingError> for Error {
+    fn from(err: MissingError) -> Self {
+        Self::Missing(err)
+    }
+}
+
+impl From<InvalidUnicodeError> for Error {
+    fn from(err: InvalidUnicodeError) -> Self {
+        Self::InvalidUnicode(err)
+    }
+}
+
+impl From<ParseError> for Error {
+    fn from(err: ParseError) -> Self {
+        Self::Parse(err)
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct MissingError {
     pub key: String,
 }
 
+impl Display for MissingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        unimplemented!()
+    }
+}
+
+impl StdError for MissingError {}
+
+#[derive(Debug, Clone)]
 pub struct InvalidUnicodeError {
     pub key: String,
     pub value: String,
 }
 
+impl Display for InvalidUnicodeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        unimplemented!()
+    }
+}
+
+impl StdError for InvalidUnicodeError {}
+
+#[derive(Debug)]
 pub struct ParseError {
     pub key: String,
     pub value: String,
@@ -26,18 +84,10 @@ pub struct ParseError {
     pub err: Box<dyn StdError>,
 }
 
-impl From<MissingError> for Error {
-    fn from(err: MissingError) -> Self {
-        Self::Missing(err)
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        unimplemented!()
     }
 }
-impl From<InvalidUnicodeError> for Error {
-    fn from(err: InvalidUnicodeError) -> Self {
-        Self::InvalidUnicode(err)
-    }
-}
-impl From<ParseError> for Error {
-    fn from(err: ParseError) -> Self {
-        Self::Parse(err)
-    }
-}
+
+impl StdError for ParseError {}
